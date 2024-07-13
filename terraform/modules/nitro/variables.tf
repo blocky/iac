0b95c2b296b0f28f4c369eaf6f581b84da5ca88f
David Millman <dave@cs.unc.edu>
2024-07-13 00:42:04 -0600

    Inital version with demo

---
+++ terraform/modules/nitro/variables.tf
@@ -0,0 +1,9 @@
+variable "instance_name" {
+  description = "The name of the instance"
+  type        = string
+}
+
+variable "key_pair_name" {
+  description = "The name of the key pair"
+  type        = string
+}
